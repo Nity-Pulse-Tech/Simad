45940c705ba1e4bebc0b4493b633547451cee277
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-03-10 21:11:37 +0100

    feat: Update database configuration to use individual PostgreSQL environment variables and add a corresponding .env.example file.

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,7 +46,16 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB", default="simad"),
+        "USER": env("POSTGRES_USER", default="simad"),
+        "PASSWORD": env("POSTGRES_PASSWORD", default="simad"),
+        "HOST": env("POSTGRES_HOST", default="localhost"),
+        "PORT": env("POSTGRES_PORT", default="5432"),
+    }
+}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
