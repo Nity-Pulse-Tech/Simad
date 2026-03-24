ea87b0554f6744d5511a2e6313538cea325886e5
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-03-24 21:25:24 +0100

    feat: Introduce conditional execution for entrypoint commands and enable PostgreSQL Trigram extension.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,14 +4,20 @@ set -e  # Exit immediately if a command exits with a non-zero status
 echo "📡 Waiting for the database to be ready..."
 sleep 5
 
-echo "🛠️ Running migrations..."
-uv run python manage.py migrate --noinput
+if [ "$RUN_MIGRATIONS" = "1" ]; then
+    echo "🛠️ Running migrations..."
+    uv run python manage.py migrate --noinput
+fi
 
-echo "📦 Collecting static files..."
-uv run python manage.py collectstatic --noinput --clear
+if [ "$COLLECT_STATIC" = "1" ]; then
+    echo "📦 Collecting static files..."
+    uv run python manage.py collectstatic --noinput --clear
+fi
 
-echo "🌐 Compiling translations..."
-uv run python manage.py compilemessages -i .venv -i node_modules -l en # English
+if [ "$COMPILE_MESSAGES" = "1" ]; then
+    echo "🌐 Compiling translations..."
+    uv run python manage.py compilemessages -i .venv -i node_modules -l en # English
+fi
 
 echo "✅ Translation Compilation Complete"
 
