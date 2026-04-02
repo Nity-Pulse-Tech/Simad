3c356f2191141bdbc449da1ae303326e8de358d4
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-04-02 21:47:09 +0100

    feat: import models from django.db in admin_views.py

--- simad/core/views/admin_views.py
+++ simad/core/views/admin_views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
-from django.db import transaction
+from django.db import transaction, models
 from django.shortcuts import redirect, get_object_or_404
 from django.utils.text import slugify
 
